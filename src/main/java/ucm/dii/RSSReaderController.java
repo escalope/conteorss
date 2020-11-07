73b9bb4528ce89e35915d586b3401ee446ae2fa7
Jorge Gomez <jjgomez@ucm.es>
2020-11-07 20:53:26 +0100

    testing

--- src/main/java/ucm/dii/RSSReaderController.java
+++ src/main/java/ucm/dii/RSSReaderController.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import org.w3c.dom.events.EventTarget;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -17,10 +19,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.imageio.ImageIO;
 import javax.management.Notification;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.statistics.HistogramDataset;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -48,11 +58,74 @@ public class RSSReaderController {
 	return ""+(count-1);
 	}   
 
-@RequestMapping("/rss/resumenensayo")
-	public String getSummary1() throws IOException {		
-	     
-     
-	return  "<b>hola</b>";
+	@GetMapping(value = "/rss/generacsv", produces = "text/csv")
+	public ResponseEntity getCSV() throws IOException {		
+		String item="sostenibilidad";
+		 // code from https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
+		 URL url = new URL("https://tecnologiasostenibilidad.blogspot.com/feeds/posts/default/-/"+item+"?alt=rss");
+	     BufferedReader in = new BufferedReader(
+	     new InputStreamReader(url.openStream()));
+	     String inputLine;     
+	     int count=0;
+	     StringBuffer sb=new StringBuffer();
+	     while ((inputLine = in.readLine()) != null)
+	    	 sb.append(inputLine);
+	     in.close();	 
+	     String contenido=sb.toString();
+
+	     String contenidoODS="";
+	    for (int k=1;k<=17;k++){
+	        contenidoODS=contenidoODS+"ODS"+k+","+contenido.split("ODS"+k+"<").length+"\n";
+	    }
+	    String fichero="ODS.csv";
+	    
+	   return  ResponseEntity.ok()
+        .header("Content-Disposition", "attachment; filename=" + fichero )
+        .contentLength(contenidoODS.getBytes().length)
+        .contentType(MediaType.parseMediaType("text/csv"))
+        .body(new ByteArrayResource(contenidoODS.getBytes()));
+	     		
+	
+	}     
+	
+	
+	@RequestMapping(value = "/rss/generachart")
+	public ResponseEntity getChart() throws IOException {	
+		final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        	 
+		 
+		String item="sostenibilidad";
+		 // code from https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
+		 URL url = new URL("https://tecnologiasostenibilidad.blogspot.com/feeds/posts/default/-/"+item+"?alt=rss");
+	     BufferedReader in = new BufferedReader(
+	     new InputStreamReader(url.openStream()));
+	     String inputLine;     
+	     int count=0;
+	     StringBuffer sb=new StringBuffer();
+	     while ((inputLine = in.readLine()) != null)
+	    	 sb.append(inputLine);
+	     in.close();	 
+	     String contenido=sb.toString();
+
+	   
+	    for (int k=1;k<=17;k++){
+	    	 dataset.addValue(contenido.split("ODS"+k+"<").length, "ODS", "ODS"+k);
+	    }
+	    
+	    
+	    JFreeChart chart = ChartFactory.createBarChart(
+				 "Progreso ODS",
+	                "ODS", "#Acciones", dataset,
+	                PlotOrientation.VERTICAL,
+	                true,
+	                true,
+	                false);
+	    chart.setBackgroundPaint(Color.white);
+	    BufferedImage img = chart.createBufferedImage(600, 300); 
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    ImageIO.write(img, "PNG", baos);
+	    return new ResponseEntity<byte[]>(baos.toByteArray(),
+	    		HttpStatus.OK);
 	
 	}     
 	
